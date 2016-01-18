318b4ce3b190db14c12a00ba4efb63a6708398f1
liangjiapei1103 <liang98@purdue.edu>
2016-01-18 10:47:46 -0500

    add header footer and register view template

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app) {
+	app.get('/', function(req, res, next) {
+	  res.render('index', { 
+	  	title: 'Blog' 
+	  });
+	});
+
+	app.get('/register', function (req, res) {
+		res.render('register', {
+			title: 'Register'
+		});
+	});
+}
 
 module.exports = router;
