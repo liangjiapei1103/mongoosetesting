2b1ef1a943b321519325cb866b385dcdd46375cf
liangjiapei1103 <liang98@purdue.edu>
2016-01-18 10:40:25 -0500

    add mongoose use

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +27,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+var url = 'mongodb://liangjiapei1103:12345678@ds047075.mongolab.com:47075/heroku_ckxw7w9x'
+mongoose.connect('url');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongo connected');
+});
+
+app.use(session({
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
